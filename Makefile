d296f5758cb3b9617224ab7228037e90d70df752
Evgeny <info@tinamagic.lv>
2024-08-09 16:34:06 +0300

    Step 1

---
+++ Makefile
@@ -0,0 +1,15 @@
+PORT ?= 8000
+
+install:
+	poetry install
+
+dev:
+	poetry run flask --app page_analyzer:app --debug run
+
+
+lint:
+	poetry run flake8 page_analyzer
+
+
+start:
+	poetry run gunicorn -w 5 -b 0.0.0.0:$(PORT) page_analyzer:app
---
+++ page_analyzer/app.py
@@ -0,0 +1,14 @@
+import os
+
+from flask import Flask
+from dotenv import load_dotenv
+
+
+load_dotenv()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+
+@app.route('/')
+def hello_world():
+    return 'Welcome to Flask!'
